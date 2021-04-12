ad1c2d2fba1c900479fa5a82c1f8c91f8dfd9ad6
rahul kathait <edendattox@gmail.com>
2021-04-12 18:45:01 +0530

    almost completed

---
+++ Response.js
@@ -0,0 +1,463 @@
+export default {
+  kind: "customsearch#search",
+  url: {
+    type: "application/json",
+    template:
+      "https://www.googleapis.com/customsearch/v1?q={searchTerms}&num={count?}&start={startIndex?}&lr={language?}&safe={safe?}&cx={cx?}&sort={sort?}&filter={filter?}&gl={gl?}&cr={cr?}&googlehost={googleHost?}&c2coff={disableCnTwTranslation?}&hq={hq?}&hl={hl?}&siteSearch={siteSearch?}&siteSearchFilter={siteSearchFilter?}&exactTerms={exactTerms?}&excludeTerms={excludeTerms?}&linkSite={linkSite?}&orTerms={orTerms?}&relatedSite={relatedSite?}&dateRestrict={dateRestrict?}&lowRange={lowRange?}&highRange={highRange?}&searchType={searchType}&fileType={fileType?}&rights={rights?}&imgSize={imgSize?}&imgType={imgType?}&imgColorType={imgColorType?}&imgDominantColor={imgDominantColor?}&alt=json",
+  },
+  queries: {
+    request: [
+      {
+        title: "Google Custom Search - s",
+        totalResults: "551000000",
+        searchTerms: "s",
+        count: 10,
+        startIndex: 1,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "e72eb0e281a86e282",
+      },
+    ],
+    nextPage: [
+      {
+        title: "Google Custom Search - s",
+        totalResults: "551000000",
+        searchTerms: "s",
+        count: 10,
+        startIndex: 11,
+        inputEncoding: "utf8",
+        outputEncoding: "utf8",
+        safe: "off",
+        cx: "e72eb0e281a86e282",
+      },
+    ],
+  },
+  context: {
+    title: "Google",
+  },
+  searchInformation: {
+    searchTime: 0.578803,
+    formattedSearchTime: "0.58",
+    totalResults: "551000000",
+    formattedTotalResults: "551,000,000",
+  },
+  items: [
+    {
+      kind: "customsearch#result",
+      title: "Google Slides: Free Online Presentations for Personal Use",
+      htmlTitle: "Google Slides: Free Online Presentations for Personal Use",
+      link: "https://www.google.com/slides/about/",
+      displayLink: "www.google.com",
+      snippet:
+        "Create a new presentation and edit it with others at the same time — from your \ncomputer, phone or tablet. Free with a Google account.",
+      htmlSnippet:
+        "Create a new presentation and edit it with others at the same time — from your <br>\ncomputer, phone or tablet. Free with a Google account.",
+      cacheId: "BqbRM4Tdrv4J",
+      formattedUrl: "https://www.google.com/slides/about/",
+      htmlFormattedUrl: "https://www.google.com/slides/about/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src:
+              "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSPhgKD4ebgQqebcB5_Qjs5ikR4PGAEZrpa_0_TpZq8UpIqXjhL9FNH4A",
+            width: "120",
+            height: "120",
+          },
+        ],
+        metatags: [
+          {
+            viewport:
+              "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no",
+          },
+        ],
+        cse_image: [
+          {
+            src:
+              "https://www.google.com/slides/about/images/common/add-pam.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "10 S Dearborn St, 10 S Dearborn St, Chicago, IL, 60603, us ...",
+      htmlTitle:
+        "10 <b>S</b> Dearborn St, 10 <b>S</b> Dearborn St, Chicago, IL, 60603, us ...",
+      link:
+        "https://www.google.com/maps/search/?api=1&query=10+S+Dearborn+St%2C+10+S+Dearborn+St%2C+Chicago%2C+IL%2C+60603%2C+us&query_place_id=ChIJk_EgbLssDogRDplS3-Km3ns",
+      displayLink: "www.google.com",
+      snippet:
+        "10 S Dearborn St, 10 S Dearborn St, Chicago, IL, 60603, us. Connect to internet \nto see place info.",
+      htmlSnippet:
+        "10 <b>S</b> Dearborn St, 10 <b>S</b> Dearborn St, Chicago, IL, 60603, us. Connect to internet <br>\nto see place info.",
+      cacheId: "SbT58pZj_oYJ",
+      formattedUrl: "https://www.google.com/maps/search/?api=1...S...S...",
+      htmlFormattedUrl:
+        "https://www.google.com/maps/search/?api=1...<b>S</b>...<b>S</b>...",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQUDTMfgWSotdHvbOKW4AmY1steqsilnDJS4q33zGx-d0AXvvxPiFY9Mvs",
+            width: "204",
+            height: "247",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            "twitter:card": "summary",
+            "theme-color": "#3367d6",
+            viewport: "initial-scale=1.0, user-scalable=no",
+            google: "notranslate",
+          },
+        ],
+        cse_image: [
+          {
+            src:
+              "https://maps.gstatic.com/tactile/basepage/pegman_sherlock.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google Sheets: Free Online Spreadsheets for Personal Use",
+      htmlTitle: "Google Sheets: Free Online Spreadsheets for Personal Use",
+      link: "https://www.google.com/sheets/about/",
+      displayLink: "www.google.com",
+      snippet:
+        "Create a new spreadsheet and edit it with others at the same time — from your \ncomputer, phone or tablet. Free with a Google account.",
+      htmlSnippet:
+        "Create a new spreadsheet and edit it with others at the same time — from your <br>\ncomputer, phone or tablet. Free with a Google account.",
+      cacheId: "95sduKEJz2gJ",
+      formattedUrl: "https://www.google.com/sheets/about/",
+      htmlFormattedUrl: "https://www.google.com/sheets/about/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src:
+              "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTjq4k4sWzbztEvmrxoHiZT_zkivYyExW6N5b_4S41Vn9UJCL1OTAsXXQ",
+            width: "120",
+            height: "120",
+          },
+        ],
+        metatags: [
+          {
+            viewport:
+              "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no",
+          },
+        ],
+        cse_image: [
+          {
+            src:
+              "https://www.google.com/sheets/about/images/common/add-tom.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title:
+        "Lowe`s Companies Inc (LOW : NYSE) Stock Price & News - Google ...",
+      htmlTitle:
+        "Lowe`<b>s</b> Companies Inc (LOW : NYSE) Stock Price &amp; News - Google ...",
+      link: "https://www.google.com/finance/quote/LOW:NYSE",
+      displayLink: "www.google.com",
+      snippet:
+        "Get the latest Lowe`s Companies Inc (LOW) real-time quote, historical \nperformance, charts, and other financial information to help you make more \ninformed ...",
+      htmlSnippet:
+        "Get the latest Lowe`<b>s</b> Companies Inc (LOW) real-time quote, historical <br>\nperformance, charts, and other financial information to help you make more <br>\ninformed&nbsp;...",
+      cacheId: "6vxA54cwZ60J",
+      formattedUrl: "https://www.google.com/finance/quote/LOW:NYSE",
+      htmlFormattedUrl: "https://www.google.com/finance/quote/LOW:NYSE",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src:
+              "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRQldTHAz14MiWY-2bpICyONYLUjcoY7hJc8AQj1tmfmhHDQ43UNmSVu_8",
+            width: "311",
+            height: "162",
+          },
+        ],
+        metatags: [
+          {
+            "application-name": "Google Finance",
+            "og:image":
+              "https://ssl.gstatic.com/finance/favicon/finance_770x402.png",
+            "theme-color": "#ffffff",
+            "og:type": "website",
+            "og:image:width": "770",
+            "twitter:card": "summary",
+            "twitter:title":
+              "Lowe`s Companies Inc (LOW : NYSE) Stock Price & News - Google Finance",
+            "apple-mobile-web-app-title": "Google Finance",
+            "og:title":
+              "Lowe`s Companies Inc (LOW : NYSE) Stock Price & News - Google Finance",
+            "og:image:height": "402",
+            "og:description":
+              "Get the latest Lowe`s Companies Inc (LOW) real-time quote, historical performance, charts, and other financial information to help you make more informed trading and investment decisions.",
+            "twitter:image":
+              "https://ssl.gstatic.com/finance/favicon/finance_496x496.png",
+            referrer: "origin",
+            "apple-mobile-web-app-status-bar-style": "default",
+            "msapplication-tap-highlight": "no",
+            "twitter:site": "@google",
+            viewport:
+              "width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no,minimal-ui",
+            "apple-mobile-web-app-capable": "yes",
+            "twitter:description":
+              "Get the latest Lowe`s Companies Inc (LOW) real-time quote, historical performance, charts, and other financial information to help you make more informed trading and investment decisions.",
+            "mobile-web-app-capable": "yes",
+            "og:url": "https://www.google.com/finance/quote/LOW:NYSE",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://ssl.gstatic.com/finance/favicon/finance_770x402.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google Forms: Free Online Surveys for Personal Use",
+      htmlTitle: "Google Forms: Free Online Surveys for Personal Use",
+      link: "https://www.google.com/forms/about/",
+      displayLink: "www.google.com",
+      snippet:
+        "Create a new survey and edit it with others at the same time. Choose from a \nvariety of pre-made themes or create your own. Free with a Google account.",
+      htmlSnippet:
+        "Create a new survey and edit it with others at the same time. Choose from a <br>\nvariety of pre-made themes or create your own. Free with a Google account.",
+      cacheId: "DQBEwPZfQ7YJ",
+      formattedUrl: "https://www.google.com/forms/about/",
+      htmlFormattedUrl: "https://www.google.com/forms/about/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src:
+              "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcTBy2Hf-wJ3myS37Oe7PpUH7G4PIGfcF08ZRJ0ZfMczLnQ-r2Mt4wao",
+            width: "120",
+            height: "120",
+          },
+        ],
+        metatags: [
+          {
+            viewport:
+              "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.google.com/forms/about/images/common/add-tom.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google",
+      htmlTitle: "Google",
+      link: "https://www.google.com/imghp?hl=EN",
+      displayLink: "www.google.com",
+      snippet: "Google Images. The most comprehensive image search on the web.",
+      htmlSnippet:
+        "Google Images. The most comprehensive image search on the web.",
+      cacheId: "ZY5ouBM3GeMJ",
+      formattedUrl: "https://www.google.com/imghp?hl=EN",
+      htmlFormattedUrl: "https://www.google.com/imghp?hl=EN",
+      pagemap: {
+        metatags: [
+          {
+            referrer: "origin",
+            viewport: "width=device-width,minimum-scale=1.0",
+            google: "notranslate",
+            "format-detection": "telephone=no",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google",
+      htmlTitle: "Google",
+      link: "https://www.google.com/",
+      displayLink: "www.google.com",
+      snippet:
+        "Search the world's information, including webpages, images, videos and more. \nGoogle has many special features to help you find exactly what you're looking ...",
+      htmlSnippet:
+        "Search the world&#39;s information, including webpages, images, videos and more. <br>\nGoogle has many special features to help you find exactly what you&#39;re looking&nbsp;...",
+      cacheId: "y14FcUQOGl4J",
+      formattedUrl: "https://www.google.com/",
+      htmlFormattedUrl: "https://www.google.com/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src:
+              "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcT3X9ZpkcNrr2kAcGZuXPZTNiK1oeTXhRMCZ1UGxNLf-an4du0eriX01mc",
+            width: "320",
+            height: "112",
+          },
+        ],
+        metatags: [
+          {
+            referrer: "origin",
+            viewport: "width=device-width,minimum-scale=1.0",
+            google: "notranslate",
+            "format-detection": "telephone=no",
+          },
+        ],
+        cse_image: [
+          {
+            src:
+              "https://www.google.com/images/branding/googlelogo/2x/googlelogo_color_160x56dp.png",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Waymo",
+      htmlTitle: "Waymo",
+      link: "http://www.google.com/selfdrivingcar/",
+      displayLink: "www.google.com",
+      snippet:
+        "Waymo—formerly the Google self-driving car project—stands for a new way \nforward in mobility. Our mission is to make it safe and easy for people and things \nto ...",
+      htmlSnippet:
+        "Waymo—formerly the Google self-driving car project—stands for a new way <br>\nforward in mobility. Our mission is to make it safe and easy for people and things <br>\nto&nbsp;...",
+      cacheId: "jH6OCbvTOPQJ",
+      formattedUrl: "www.google.com/selfdrivingcar/",
+      htmlFormattedUrl: "www.google.com/selfdrivingcar/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src:
+              "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQDe3q_ecCo3327WH9qZ7L8ws21NE7bS3jFwMQ03gCBs4uCnT6WVePFQPA",
+            width: "310",
+            height: "163",
+          },
+        ],
+        metatags: [
+          {
+            "og:image": "https://waymo.com/v2/static/images/waymo_banner.jpg",
+            "og:type": "website",
+            "twitter:card": "summary_large_image",
+            "twitter:title": "Waymo",
+            "og:site_name": "Waymo",
+            "og:title": "Waymo",
+            "og:description":
+              "Waymo—formerly the Google self-driving car project—stands for a new way forward in mobility. Our mission is to make it safe and easy for people and things to move around.",
+            "twitter:creator": "@Waymo",
+            "og:image:secure_url":
+              "https://waymo.com/v2/static/images/waymo_banner.jpg",
+            "twitter:image":
+              "https://waymo.com/v2/static/images/waymo_banner.jpg",
+            referrer: "no-referrer",
+            "twitter:site": "@Waymo",
+            viewport: "width=device-width, initial-scale=1.0",
+            "twitter:description":
+              "Waymo—formerly the Google self-driving car project—stands for a new way forward in mobility. Our mission is to make it safe and easy for people and things to move around.",
+            "og:locale": "en",
+            "og:url": "https://waymo.com/",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://waymo.com/v2/static/images/waymo_banner.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google Docs: Free Online Documents for Personal Use",
+      htmlTitle: "Google Docs: Free Online Documents for Personal Use",
+      link: "https://www.google.com/docs/about/",
+      displayLink: "www.google.com",
+      snippet:
+        "Create a new document and edit it with others at the same time — from your \ncomputer, phone or tablet. Free with a Google account.",
+      htmlSnippet:
+        "Create a new document and edit it with others at the same time — from your <br>\ncomputer, phone or tablet. Free with a Google account.",
+      cacheId: "B4Skq4M5akkJ",
+      formattedUrl: "https://www.google.com/docs/about/",
+      htmlFormattedUrl: "https://www.google.com/docs/about/",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src:
+              "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQhFqRE2gLrcYQFWHwv1e40obIYTwULT__yZcx3AWMhPOEZV7xqU_5_",
+            width: "120",
+            height: "120",
+          },
+        ],
+        metatags: [
+          {
+            viewport:
+              "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no",
+          },
+        ],
+        cse_image: [
+          {
+            src: "https://www.google.com/docs/about/images/common/add-pam.jpg",
+          },
+        ],
+      },
+    },
+    {
+      kind: "customsearch#result",
+      title: "Google Flights",
+      htmlTitle: "Google Flights",
+      link: "https://www.google.com/flights",
+      displayLink: "www.google.com",
+      snippet:
+        "Find cheap flights in seconds, explore destinations on a map, and sign up for fare \nalerts on Google Flights.",
+      htmlSnippet:
+        "Find cheap flights in seconds, explore destinations on a map, and sign up for fare <br>\nalerts on Google Flights.",
+      cacheId: "tP7SLL9AwsYJ",
+      formattedUrl: "https://www.google.com/flights",
+      htmlFormattedUrl: "https://www.google.com/flights",
+      pagemap: {
+        cse_thumbnail: [
+          {
+            src:
+              "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRymTE8JLYFp2yDTmPNr0PcXeipbyCq1o7iGVfwp6uZQIZW2JmSdUeInS_1",
+            width: "200",
+            height: "200",
+          },
+        ],
+        metatags: [
+          {
+            "application-name": "Travel",
+            "og:image":
+              "https://ssl.gstatic.com/travel-frontend/images/travel_logo_200.png",
+            "og:type": "website",
+            "og:image:width": "200",
+            "og:image:alt": "Google travel suitcase logo",
+            mod: "true",
+            "og:title": "Travel - Google",
+            "apple-mobile-web-app-title": "Travel",
+            "og:image:height": "200",
+            "og:image:type": "image/png",
+            "og:description":
+              "Plan your trip with Google. Find flights, hotels, vacation rentals, things to do, and more.",
+            referrer: "origin",
+            "apple-mobile-web-app-status-bar-style": "black",
+            "msapplication-tap-highlight": "no",
+            viewport:
+              "width=device-width,initial-scale=1.0,minimum-scale=1.0,maximum-scale=1.0,user-scalable=no",
+            "apple-mobile-web-app-capable": "yes",
+            "mobile-web-app-capable": "yes",
+            "og:url": "https://www.google.com/travel/",
+          },
+        ],
+        cse_image: [
+          {
+            src:
+              "https://ssl.gstatic.com/travel-frontend/images/travel_logo_200.png",
+          },
+        ],
+      },
+    },
+  ],
+};
---
+++ pages/search.js
@@ -0,0 +1,47 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import { API_KEY, CONTEXT_KEY } from "../keys";
+import Response from "../Response";
+import { useRouter } from "next/router";
+import SearchResults from "../components/SearchResults";
+
+const Search = ({ results }) => {
+  const router = useRouter();
+
+  console.log(results);
+
+  return (
+    <div>
+      <Head>
+        <title>{router.query.term} - Google Search</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      {/* Header */}
+      <Header />
+      {/* Search page */}
+      <SearchResults results={results} />
+    </div>
+  );
+};
+
+export default Search;
+
+export async function getServerSideProps(context) {
+  const useDummyData = false;
+  const startIndex = context.query.start || "0";
+
+  const data = useDummyData
+    ? Response
+    : await fetch(
+        `https://www.googleapis.com/customsearch/v1?key=${API_KEY}&cx=${CONTEXT_KEY}&q=${context.query.term}`
+      ).then((res) => res.json());
+
+  //after the server has rendered.... pass the Results to the client..
+
+  return {
+    props: {
+      results: data,
+    },
+  };
+}

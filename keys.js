22014f5a697360dc4a78e7c54740bf7d842db186
rahul kathait <edendattox@gmail.com>
2021-04-17 14:26:33 +0530

    ignore damn keys

--- keys.js
+++
@@ -1,5 +0,0 @@
-//API key - https://developers.google.com/custom-search/v1/using_rest
-export const API_KEY = "AIzaSyA5wEVqGvnVvUJF0A_4tUsV5YqPeNvPV6A";
-
-//Context key - https://cse.google.com/cse/create/new
-export const CONTEXT_KEY = "e72eb0e281a86e282";
